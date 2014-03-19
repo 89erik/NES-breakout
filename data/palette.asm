c5809db3697212a20a2ef37a22f7fa014f1b2268
Erik Lothe <eriklot@stud.ntnu.no>
2014-03-19 21:07:05 +0100

    Changes background w/ palette

--- data/palette.asm
+++ data/palette.asm
@@ -1,12 +1,12 @@
 palette:
     ; Background
-    .byte 0, $12, $3C, $09
-    .byte 0, $15, $3C, $09
-    .byte 0, $15, $3C, $09
-    .byte 0, $15, $3C, $09
+    .byte $3F, $20, $3C, $09
+    .byte $3F, $15, $3C, $09
+    .byte $3F, $15, $3C, $09
+    .byte $3F, $15, $3C, $09
     
     ; Sprites
-    .byte $11, $2A, $28, $15
-    .byte $11, $15, $3C, $09
-    .byte $11, $3F, $37, $00
-    .byte $11, 0, 0, 0
+    .byte $3F, $2A, $28, $15
+    .byte $3F, $15, $3C, $09
+    .byte $3F, $3F, $37, $00
+    .byte $3F, 0, 0, 0
