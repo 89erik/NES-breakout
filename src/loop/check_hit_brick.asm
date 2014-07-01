1e8051c2ae1bbf264be7a45c8b41e2ff6e7adbcb
Erik Lothe <eriklot@stud.ntnu.no>
2014-07-01 11:48:11 +0200

    Continues implementing check hit brick
    Now detects if a brick is hit. Still no bounce.

--- src/loop/check_hit_brick.asm
+++ src/loop/check_hit_brick.asm
@@ -1,7 +1,9 @@
 CheckHitBrick:
-    TYA
-    PHA ; Preserve Y
-    LDY #0
+    TXA
+    PHA ; Preserve X
+    LDX #0
+    CPX n_bricks
+    BEQ @no_hits
     @iterate_bricks:
         LDA brick_present, X
         BNE @no_hit
@@ -10,63 +12,78 @@ CheckHitBrick:
             BNE @no_hit
             JSR @InBoundsVertical
             BNE @no_hit
-        @hit:
+            @hit:
+                PLA
+                TAX ; Retrieve X
+                LDA #TRUE
+                RTS ; Return        
+            @no_hit:
+                INX
+                CPX n_bricks
+                BCC @iterate_bricks
+        @no_hits:
             PLA
-            TAY ; Retrieve Y
-            LDA #TRUE
-            RTS ; Return
-        
-        @no_hit:
-            INY
-            CPY n_bricks
-            BCC @iterate_bricks
-            PLA
-            TAY ; Retrieve Y
+            TAX ; Retrieve X
             LDA #FALSE
             RTS ; Return
        
         
-        
     @InBoundsHorizontal:
+        @calculate_brick_coordinate_x:
+            TXA
+            PHA     ; Preserve X
+            LDA brick_x, X
+            LDX #SPRITE_SIZE
+            JSR Multiply
         @in_bounds_left:
-            LDA brick_x, X
             CLC
-            ADC #SPRITE_SIZE
+            ADC #SPRITE_SIZE-1
             CMP ball_x
-            BCC @off_left
+            BCC @off_right
         @in_bounds_right:
-            LDA brick_x, X
             SEC
-            SBC #SPRITE_SIZE
-            BCS @off_right
+            SBC #SPRITE_SIZE*2
+            BCC @off_left
             CMP ball_x
-            BCS @off_right
+            BCS @off_left
+        PLA
+        TAX     ; Retrieve X
         LDA #TRUE
         RTS
         
         @off_left:
         @off_right:
+            PLA
+            TAX     ; Retrieve X
             LDA #FALSE
             RTS
             
     @InBoundsVertical:
-        @in_bounds_top:
+        @calculate_brick_coordinate_y:
+            TXA
+            PHA     ; Preserve X
             LDA brick_y, X
+            LDX #SPRITE_SIZE
+            JSR Multiply
+        @in_bounds_top:
             CLC
-            ADC #SPRITE_SIZE
+            ADC #SPRITE_SIZE-2
             CMP ball_y
             BCC @ball_below
         @in_bounds_below:
-            LDA brick_y, X
             SEC
-            SBC #SPRITE_SIZE
-            BCS @ball_above
+            SBC #SPRITE_SIZE*2
+            BCC @ball_above
             CMP ball_y
             BCS @ball_above
+        PLA
+        TAX     ; Retrieve X
         LDA #TRUE
         RTS
         
         @ball_below:
         @ball_above:
+            PLA
+            TAX     ; Retrieve X
             LDA #FALSE
             RTS
